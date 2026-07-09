845391d4d2d02026ba5e900f337afcb362627acb
Raritanium <the3thorns@gmail.com>
2026-07-09 18:58:46 +0200

    Restores RecurrentCodec as template parameter and removed unused method overrides

--- src/core/TimeSeries.h
+++ src/core/TimeSeries.h
@@ -15,24 +15,19 @@ namespace tsdb::core {
 class TimeSeries {
 
 public:
-  enum class Error {
-    Ok,
-    AppendError,
-    InvalidRange,
-  };
 
   static constexpr int block_count = 12;
   static constexpr int block_period = 24 / block_count;
 
-  template <TimeMarkRecurrentCodec C>
+  template <RecurrentCodec C>
   TimeSeries() : pimpl_{ std::make_unique<TimeSeriesModel<C>>() } {}
 
-  template <TimeMarkRecurrentCodec C>
+  template <RecurrentCodec C>
   TimeSeries(TimeSeries&& other) noexcept : pimpl_{ std::make_unique<C>(std::move(other.pimpl_)) } {}
 
 private:
 
-  template <TimeMarkRecurrentCodec C>
+  template <RecurrentCodec C>
   struct Block {
     BitBuffer buffer{};
     C codec{};
@@ -46,11 +41,11 @@ private:
   struct TimeSeriesConcept {
     virtual ~TimeSeriesConcept() = default;
 
-    virtual Error append(TimeMark mark) = 0;
-    virtual Error query_range(timestamp from, timestamp to) = 0;
+    virtual void append(TimeMark mark) = 0;
+    virtual void query_range(timestamp from, timestamp to) = 0;
   };
 
-  template <TimeMarkRecurrentCodec C>
+  template <RecurrentCodec C>
   class TimeSeriesModel : public TimeSeriesConcept {
   public:
 
@@ -58,39 +53,6 @@ private:
       for (int i = 0; i < block_count; i+=1) {
         blocks_[i] = Block<C>{static_cast<timestamp>(i * block_period)};
       }
-    }
-
-    Error append(TimeMark mark) override {
-      // Check time value
-      auto hour = mark.time % 86400;
-      if (hour % 2 != 0) {
-        hour -= 1;
-      }
-      auto& block = blocks_[hour];
-      auto err = block.codec.compress(mark, block.buffer);
-      if (err != CodecError::Ok) {
-        return Error::AppendError;
-      }
-      return Error::Ok;
-    };
-
-    Error query_range(const timestamp from, const timestamp to) override {
-      if (from > to) return Error::InvalidRange;
-
-      auto from_hour = from % 86400;
-      auto indexed_from_hour = from_hour;
-      if (indexed_from_hour % 2 == 0) {
-        indexed_from_hour -= 1;
-      }
-      auto to_hour = to % 86400;
-      auto indexed_to_hour = to_hour;
-      if (indexed_to_hour % 2 == 0) {
-        indexed_to_hour -= 1;
-      }
-
-      // Retrieve a range of records
-
-      return Error::Ok;
     }
 
   private:
